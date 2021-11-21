954f6be6d94ad070a4b513d0f4c18932bd9d169e
Mitchell Adair <mitchwadair@gmail.com>
2021-11-20 22:12:17 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: ./build.sh v0.0.0"
+  exit 1
+fi
+
 uglifyjs --compress --mangle --comments --output ./dist/clipnship.min.js --source-map "filename='./dist/clipnship.js.map'" ./src/clipnship.js
+
+git add dist/clipnship.min.js dist/clipnship.min.js.map
+git commit -m $1
